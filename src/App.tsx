045c20c556aa477baf018638264a4770a9528bec
Matt Mazzola <mattm@microsoft.com>
2021-10-07 13:45:50 -0700

    Add request to unhandled endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 function App() {
 
-  const [response, setResponse] = React.useState('');
+  const [response1, setResponse1] = React.useState('');
 
   const onClickSend = async () => {
-
     const response = await fetch('/api/results', {
       method: 'POST',
       headers: {
@@ -18,11 +17,30 @@ function App() {
 
     if (response.ok) {
       const json = await response.json();
-      setResponse(json);
+      setResponse1(json);
     }
     else {
       const responseText = `${response.status} ${response.statusText}`;
-      setResponse(responseText)
+      setResponse1(responseText)
+    }
+  };
+
+  const [response2, setResponse2] = React.useState('');
+  const onClickSend2 = async () => {
+    const response = await fetch('/api/unhandled', {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+      }
+    });
+
+    if (response.ok) {
+      const json = await response.json();
+      setResponse2(json);
+    }
+    else {
+      const responseText = `${response.status} ${response.statusText}`;
+      setResponse2(responseText)
     }
   };
 
@@ -36,7 +54,9 @@ function App() {
         Click to send a request and display the response
         </p>
         <button onClick={onClickSend}>Send</button>
-        <pre>{JSON.stringify(response, null, 2)}</pre>
+        <pre>{JSON.stringify(response1, null, 2)}</pre>
+        <button onClick={onClickSend2}>Send 2</button>
+        <pre>{JSON.stringify(response2, null, 2)}</pre>
       </main>
     </div>
   );
